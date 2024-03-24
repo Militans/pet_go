8153c788cdb6abffdb4f59fb3ae8a3bed3ed6697
Militans <ivangreku48@gmail.com>
2024-03-24 14:48:03 +0300

    applying orders

--- main.go
+++ main.go
@@ -1,33 +1,17 @@
 package main
 
 import (
-	"database/sql"
+	"context"
 	"fmt"
-	"github.com/go-chi/chi/v5"
-	"net/http"
+	"start_chi/application"
 )
 
 func main() {
-	db, err := sql.Open("MariaDB", "root:1q2w_1q2w@tcp(localhost:3306)/for_go")
+	app := application.New()
+
+	err := app.Start(context.TODO())
+
 	if err != nil {
-		fmt.Println("Failed connect")
+		fmt.Println("no", err)
 	}
-	defer db.Close()
-	
-	router := chi.NewRouter()
-
-	router.Get("/hello", basicHandler)
-
-	server := &http.Server{
-		Addr:    ":3000",
-		Handler: router,
-	}
-	err := server.ListenAndServe()
-	if err != nil {
-		fmt.Println("failed to listen to server", err)
-	}
-}
-
-func basicHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("hello world"))
 }
